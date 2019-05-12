df84c348cffb37b93dc6e1501dc600f8a1081ef2
cjmair <colin@mair.org.uk>
2019-05-12 13:21:52 +0100

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo
+echo This script install the prerequisits for temperhum.py on a Raspberry Pi and copies it to /usr/local/bin
+read -p "Continue [Y/N]? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+
+    set -x
+
+    sudo apt-get upgrade
+    sudo apt-get -y install python-usb python3-usb python-setuptools python3-setuptools
+    sudo cp  ./99-temperhum.rules /etc/udev/rules.d
+    sudo adduser pi plugdev
+    sudo cp ./temperhum.py /usr/local/bin
+    { set +x; } 2>&-
+                                    
+fi
+
+
